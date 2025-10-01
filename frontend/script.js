f8d03cd81f26d384a9690792530043b49f802453
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-10-01 13:05:34 +0530

    Rename login.html to index.html

--- frontend/script.js
+++ frontend/script.js
@@ -20,7 +20,7 @@ async function signup() {
     });
     const data = await res.json();
     alert(data.message || "Signup successful! Please login.");
-    window.location.href = "login.html";
+    window.location.href = "index.html";
   } catch (err) {
     console.error(err);
     alert("Signup failed.");
